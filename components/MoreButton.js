04aaebc65d5478003d9910c9202db0d9568c45d7
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2023-01-15 12:39:06 +0300

    :sparkles: new button

---
+++ components/MoreButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MoreButton = (props) => {
+  return (
+    <div className='more-button'>
+        <p className='white-mini-text'>More</p>
+        <p className='arrow'>🠧</p>
+    </div>
+  )
+}
+
+export default MoreButton
