1909e6a445fa8c959fe49a35582f8ac7dc5331bd
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2023-01-15 12:40:11 +0300

    :truck: morebutton -> morelink

---
+++ components/MoreLink.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const MoreLink = (props) => {
+    const [link,setLink] = useState(props.link);
+
+  return (
+    <Link href={link}>
+        <div className='more-button'>
+            <p className='white-mini-text'>More</p>
+            <p className='arrow'>⟶</p>
+        </div>
+    </Link>
+  )
+}
+
+export default MoreLink
