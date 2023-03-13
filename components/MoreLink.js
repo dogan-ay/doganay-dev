5cd42c3a26a3e32ab613ee47dad6a9910fad09c8
Doğan Ay Şengül <72282951+dogan-ay@users.noreply.github.com>
2023-03-14 00:58:22 +0300

    update arrow

--- components/MoreLink.js
+++ components/MoreLink.js
@@ -7,8 +7,7 @@ const MoreLink = (props) => {
   return (
     <Link href={link} style={{display: 'inline-block', width: 'min-content'}}>
         <div className='more-button'>
-            <p className='mid-mini-text'>More</p>
-            <p className='arrow'>⟶</p>
+            <p className='mid-mini-text'>More ⟶</p>
         </div>
     </Link>
   )
