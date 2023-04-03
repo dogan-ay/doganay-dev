ce30136f3620e2b28bf87b584edd327ee417fb81
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2023-04-04 01:35:03 +0300

    :sparkles: add meta tags

--- components/Meta.js
+++ components/Meta.js
@@ -1,12 +1,25 @@
 import Head from "next/head";
 import React from 'react'
 
-const Meta = ({description, title, keywords}) => {
+const Meta = (props) => {
+  const img = props.img ? props.img : '';
+
   return (
     <div>
         <Head>
-            <meta name="description" content={description}/>
-            <meta name="keywords" content={keywords}/>
+            <meta name="description" content={props.description}/>
+            <meta name="keywords" content={props.keywords}/>
+            <meta name="twitter:card" content="summary_large_image"></meta>
+            <meta name="twitter:site" content="@hereticdev" />
+            <meta name="twitter:title" content={props.title} />
+            <meta name="twitter:description" content={props.description} />
+            <meta name="twitter:image" content={img} />
+            <meta property="og:type" content="article" />
+            <meta property="og:title"  content={props.title}/>
+            <meta property="og:description" content={props.description} />
+            <meta property="og:url" content="https://doganay.dev/" />
+            <meta property="og:image" content={img} />
+
             <title>{title}</title>
             <link rel='icon' href='/favicon.ico'/>
         </Head>
